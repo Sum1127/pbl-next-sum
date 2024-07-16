42bf1d9a27f36ef46499add2ca16dd101455e9c3
Sum1127 <clownyuuran@gmail.com>
2024-07-16 10:32:48 +0900

    エラー修正１

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -13,7 +13,7 @@ export default function Hello() {
     <Box>
       <Box>
         <Heading as='h1' size='3xl' w="200px" noOfLines={1} bgGradient='linear(to-r,red.500, orange.500)' bgClip='text'>Hello!</Heading>
-        <Heading as='h2' size='md' w="180px" noOfLines={1} bgGradient='linear(to-r,blue.400, pink.400)' bgClip='text'>This is Sum's site</Heading>
+        <Heading as='h2' size='md' w="180px" noOfLines={1} bgGradient='linear(to-r,blue.400, pink.400)' bgClip='text'>This is Sum&apos;s site</Heading>
         <Heading as='h2' size='md' w="400px" noOfLines={1} bgGradient='linear(to-r,yellow.600, black)' bgClip='text'>This website is in preparation</Heading>
       </Box>
       <Box backgroundColor="blue.50">
